9d6674e6bbe40090879ae8951d5f92a6ffa0f6d5
ventivm <danibe248@gmail.com>
2021-04-04 14:50:14 +0200

    added single resource get

--- api.py
+++ api.py
@@ -4,7 +4,6 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
 books = [
     {'id': 0,
      'title': 'A Fire Upon the Deep',
@@ -29,9 +28,24 @@ def home():
     return '''This was a get!'''
 
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    results = []
+
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    return jsonify(results)
 
 app.run(host='0.0.0.0',port=55500)
