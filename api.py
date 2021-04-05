bea0539c09ab9507dc763e8cbd1dfcad8f1ca719
ventivm <danibe248@gmail.com>
2021-04-05 14:27:38 +0200

    code cleaning and functions completion

--- api.py
+++ api.py
@@ -10,33 +10,15 @@ TABLE_NAME = "questions"
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(TABLE_NAME)
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
 @app.route('/', methods=['GET'])
 def home():
-    return '''This was a get!'''
+    return '''HEALTHCHECK'''
 
 
 @app.route('/api/v1/resources/questions/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    response = table.scan()
+    return jsonify(response['Items'])
 
 
 @app.route('/api/v1/resources/questions', methods=['GET'])
